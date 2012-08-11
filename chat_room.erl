33c41de745d4d2e68eff93193ce61edfffd9375c
Luiz Branco <artfikan@gmail.com>
2012-08-11 14:34:14 -0300

    Add todo list

--- chat_room.erl
+++ chat_room.erl
@@ -1,19 +1,20 @@
+%TODO create database
 -module(chat_room).
 -export([router/0]).
 
 router() ->
   receive
-    {send_message, Name, Message} ->
-      send_message(Name, Message),
+    {send_message, Username, Message} ->
+      send_message(Username, Message),
       router();
     {request_messages, TimeStamp} ->
       request_messages(TimeStamp),
       router();
-    {join_room, Name} ->
-      join_room(Name),
+    {join_room, Username} ->
+      join_room(Username),
       router();
-    {leave_room, Name} ->
-      leave_room(Name),
+    {leave_room, Username} ->
+      leave_room(Username),
       router();
     close ->
       ok;
@@ -22,10 +23,19 @@ router() ->
       router()
   end.
 
-send_message(_, Message) -> Message.
+send_message(_, Message) ->
+  %TODO save user, message and timestamp
+  Message.
 
-request_messages(TimeStamp) -> TimeStamp.
+request_messages(TimeStamp) ->
+  %TODO request messages after given timestamp
+  %or all with none is given
+  TimeStamp.
 
-join_room(Name) -> Name.
+join_room(Username) ->
+  %TODO add username to database
+  Username.
 
-leave_room(Name) -> Name.
+leave_room(Username) ->
+  %TODO remove username from database
+  Username.
