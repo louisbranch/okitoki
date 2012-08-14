94fb583cb85962bf3b601a739b3949255a64aec1
Luiz Branco <me@luizbranco.com>
2012-08-13 23:46:16 -0300

    Rename RoomName to Room

--- chat_server.erl
+++ chat_server.erl
@@ -11,51 +11,51 @@ init() ->
   receive
     create_rooms_db ->
       ets:new(rooms, [set, named_table]);
-    {open_room, RoomName} ->
-      open_room(RoomName);
-    {close_room, RoomName} ->
-      close_room(RoomName);
-    {send_to_room, RoomName, Params} ->
-      send_to_room(RoomName, Params);
+    {open_room, Room} ->
+      open_room(Room);
+    {close_room, Room} ->
+      close_room(Room);
+    {send_to_room, Room, Params} ->
+      send_to_room(Room, Params);
     _Else ->
       %save message to log file
       io:format("~p~n", [_Else])
   end,
   init().
 
-open_room(RoomName) ->
-  Result = ets:lookup(rooms, RoomName),
+open_room(Room) ->
+  Result = ets:lookup(rooms, Room),
   case Result of
     [_] ->
-      io:format("~s room already exist~n", [RoomName]),
+      io:format("~s room already exist~n", [Room]),
       error;
     [] ->
       Pid = chat_room:start(),
-      ets:insert(rooms, {RoomName, Pid}),
-      io:format("~s room was opened~n", [RoomName]),
+      ets:insert(rooms, {Room, Pid}),
+      io:format("~s room was opened~n", [Room]),
       ok
     end.
 
-close_room(RoomName) ->
-  Result = ets:lookup(rooms, RoomName),
+close_room(Room) ->
+  Result = ets:lookup(rooms, Room),
   case Result of
     [{_, RoomPid}] ->
       RoomPid ! close,
-      ets:delete(rooms, RoomName),
-      io:format("~s room was closed~n", [RoomName]),
+      ets:delete(rooms, Room),
+      io:format("~s room was closed~n", [Room]),
       ok;
     [] ->
-      io:format("~s room doesn't exist~n", [RoomName]),
+      io:format("~s room doesn't exist~n", [Room]),
       error
     end.
 
-send_to_room(RoomName, Params) ->
-  Result = ets:lookup(rooms, RoomName),
+send_to_room(Room, Params) ->
+  Result = ets:lookup(rooms, Room),
   case Result of
     [{_, RoomPid}] ->
       RoomPid ! Params,
       ok;
     [] ->
-      io:format("~s room doesn't exist~n", [RoomName]),
+      io:format("~s room doesn't exist~n", [Room]),
       error
     end.
