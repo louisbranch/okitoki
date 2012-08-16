2a4ee067e8b1c37ca713623905fbcd797adfa496
Luiz Branco <me@luizbranco.com>
2012-08-15 22:48:28 -0300

    Add initial db chat

---
+++ src/chat_db.erl
@@ -0,0 +1,8 @@
+-module(chat_db).
+-export([start/0]).
+-record(okitoki_message, {room, username, message, timestamp}).
+
+start() ->
+  mnesia:create_schema([node()]),
+  mnesia:start(),
+  mnesia:create_table(okitoki_messages, [{attributes, record_info(fields, okitoki_message)}]).
