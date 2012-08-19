6e130c599e4c77d3a796247d8e898bbda2281393
Luiz Branco <me@luizbranco.com>
2012-08-19 00:58:37 -0300

    Add user functions

--- src/chat_user.erl
+++ src/chat_user.erl
@@ -1,6 +1,9 @@
 -module(chat_user).
--export([start/1]).
+-export([new/0,new/1]).
 -export([loop/1]).
+
+new() -> start(anonymous).
+new(Username) -> start(Username).
 
 start(Username) ->
   spawn(chat_user, loop, [Username]).
@@ -8,8 +11,20 @@ start(Username) ->
 loop(Username) ->
   receive
     {chat_message, Room, SenderName, Message} ->
-      io:format("~s received ~p from ~s @~s~n", [Username, Message, SenderName, Room]);
+      io:format("~s received ~p from ~s @~s~n", [Username, Message, SenderName, Room]),
+      loop(Username);
+    {change_username, Username} ->
+      ok,
+      loop(Username);
+    {change_username, NewUsername} ->
+      change_username(Username, NewUsername),
+      loop(Username);
+    stop ->
+      ok;
     _Else ->
-      io:format("Error: ~p~n", [_Else])
-  end,
-  loop(Username).
+      io:format("Error: ~p~n", [_Else]),
+      loop(Username)
+  end.
+
+change_username(OldUsername, NewUsername) ->
+  ok.
